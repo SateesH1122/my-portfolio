3f839d80e2c3cc176d48f95be45cbff32fa92f43
Padamu Sateesh Kumar <96760962+SateesH1122@users.noreply.github.com>
2023-07-01 22:28:05 +0530

    Add files via upload

---
+++ BMI Calculator/calculate.js
@@ -0,0 +1,33 @@
+function turnblack() {
+    document.querySelector(".navibar").style.backgroundColor = "black";
+    document.querySelector(".block").style.backgroundColor = "black";
+    document.querySelector(".contact").style.backgroundColor = "black";
+}
+function calculateBMI() {
+    var heightInput = document.getElementById("height");
+    var weightInput = document.getElementById("weight");
+    var resultDiv = document.getElementById("result");
+  
+    var height = parseFloat(heightInput.value);
+    var weight = parseFloat(weightInput.value);
+  
+    if (isNaN(height) || isNaN(weight)) {
+      resultDiv.innerHTML = "Please enter valid height and weight.";
+      return;
+    }
+  
+    var bmi = weight / ((height / 100) ** 2);
+    var category = "";
+  
+    if (bmi < 18.5) {
+      category = "Underweight";
+    } else if (bmi < 25) {
+      category = "Normal weight";
+    } else if (bmi < 30) {
+      category = "Overweight";
+    } else {
+      category = "Obese";
+    }
+  
+    resultDiv.innerHTML = "Your BMI is " + bmi.toFixed(2) + " (" + category + ")";
+  }
